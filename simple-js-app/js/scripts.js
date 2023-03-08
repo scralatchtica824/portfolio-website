2d54a16b213cd2330ed4af570a1861917c728396
scralatchtica824 <122630961+scralatchtica824@users.noreply.github.com>
2023-03-08 11:30:05 -0500

    Update scripts.js
    Practicing JS

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,3 +1,21 @@
-alert('Hello world');
+// Testing normal comment
+alert('Hello world - Click Okay to see my name!');
+/* Testing a larger comment 
+that takes up 2 or more lines */
 let age = 34;
-let name = 'Doug';
+let myName = 'Doug';
+document.write(myName);
+
+let simpleAddition = 2+2;
+document.write(simpleAddition)
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+let size2 = 100;
+let doubleSize2 = size2 * 2;
+let minSize = (doubleSize2 * 2) - (size2 / 2);
+document.write(minSize);
+
+//End comments here..
